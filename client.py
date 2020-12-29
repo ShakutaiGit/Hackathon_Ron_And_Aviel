d35ca57296f0425c015501943555c3d851a7a84a
Shakutaigit <ron.shakutai@gmail.com>
2020-12-29 13:18:34 +0200

    udp broadcast

--- client.py
+++ client.py
@@ -1,20 +1,19 @@
 from socket import *
 
-def connect_to_server():
-    print("in client function")
-    serverPort=2010
-    serverName= '192.168.14.6' #the IP you want to connect to
-    clientSocket = socket(AF_INET,SOCK_DGRAM)
-    message = b"HEY Tomer its ron and we connect server to client"
-    clientSocket.sendto(message,(serverName,serverPort))
-    modifiedMessage, serverAddress = clientSocket.recvfrom(2055)
-    print(modifiedMessage)
-    clientSocket.close()
+client = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP) # UDP
 
+# Enable port reusage so we will be able to run multiple clients and servers on single (host, port). 
+# Do not use socket.SO_REUSEADDR except you using linux(kernel<3.9): goto https://stackoverflow.com/questions/14388706/how-do-so-reuseaddr-and-so-reuseport-differ for more information.
+# For linux hosts all sockets that want to share the same address and port combination must belong to processes that share the same effective user ID!
+# So, on linux(kernel>=3.9) you have to run multiple servers and clients under one user to share the same (host, port).
+# Thanks to @stevenreddie
+# client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
 
-def main():
-    print("client begin")
-    connect_to_server()
+# Enable broadcasting mode
+client.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 
-if __name__ == "__main__":
-    main()
+client.bind(("", 37020))
+while True:
+    # Thanks @seym45 for a fix
+    data, addr = client.recvfrom(1024)
+    print("received message: %s"%data)
--- server.py
+++ server.py
@@ -1,23 +1,24 @@
+
 from socket import *
+import time
 
-def pyUDPServer():
-    print( "begin the server" )
-    serverPort = 2010
-    severSocket = socket(AF_INET,SOCK_DGRAM)
-    severSocket.bind(('',serverPort))
-    print( "The server is ready to receive" )
-    while 1:
-        print( "wait for message" )
-        messsage, clientAddress = severSocket.recvfrom(2010)
-        print ("message: " + str(messsage, 'utf-8') ) # need to be translated back to str from bytes
-        modifiedMessage = messsage.upper() # a func on the message
-        print (modifiedMessage)
-        severSocket.sendto(modifiedMessage,clientAddress)
-        pass
+server = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
 
-def main():
-    print("Hello World! this is the server!!")
-    pyUDPServer()
+# Enable port reusage so we will be able to run multiple clients and servers on single (host, port). 
+# Do not use socket.SO_REUSEADDR except you using linux(kernel<3.9): goto https://stackoverflow.com/questions/14388706/how-do-so-reuseaddr-and-so-reuseport-differ for more information.
+# For linux hosts all sockets that want to share the same address and port combination must belong to processes that share the same effective user ID!
+# So, on linux(kernel>=3.9) you have to run multiple servers and clients under one user to share the same (host, port).
+# Thanks to @stevenreddie
+# server.setsockopt(SOL_SOCKET, SO_REUSEPORT, 1)
 
-if __name__ == "__main__":
-    main()
+# Enable broadcasting mode
+server.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+
+# Set a timeout so the socket does not block
+# indefinitely when trying to receive data.
+server.settimeout(0.2)
+message = b"your very important message"
+while True:
+    server.sendto(message, ('<broadcast>', 37020))
+    print("message sent!")
+    time.sleep(1)
