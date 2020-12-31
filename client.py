31bc7c6d555353fa1d1590a69140b5f2c64aa300
aviel5002 <avielavitan@gmail.com>
2020-12-31 15:42:51 +0200

    finished client

--- client.py
+++ client.py
@@ -4,34 +4,50 @@ import time
 import msvcrt
 
 def Looking_For_Server():
+    """This function represents the state of connecting to a UDP socket and getting the TCP port
+
+    Returns:
+        [ip]: [ip of tcp server]
+        [tcp_port]: [port of tcp server]
+    """
     try:
+        # Enable broadcasting mode
+        broadcast_port=13117
         client_udp = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP) #UDP socket intiallize
         client_udp.setsockopt(SOL_SOCKET,SO_REUSEADDR,1)
         client_udp.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-        client_udp.bind(("", 13117))
-        looking_server=True
-        while looking_server: #Entering looking for server mode
+        client_udp.bind(("", broadcast_port))
+        looking_for_server=True
+        while looking_for_server: #Entering looking for server mode
             data, addr = client_udp.recvfrom(1024)
             client_udp.close() #closing the udp connection after getting a reply
             (ip, port) = addr
-            content = struct.unpack('QQQ',data)  #Unpacking the message
+            message = struct.unpack('QQQ',data)  #Unpacking the message
             print("Received offer from {} attempting to connect...".format(ip))
-            looking_server = False
-        return content,ip
+            tcp_port=message[2]
+            looking_for_server = False
+        return tcp_port,ip
     except:
         pass
 
-def TCP_Connection(content,ip):
+def TCP_Connection(tcp_port,ip):
+    """This function connects to the TCP server and sends keyboard hits back to him
+
+    Args:
+        tcp_port (int): [port of the TCP server]
+        ip (int): [ip of the TCP server]
+    """
     try:
         connecting_to_tcp_server = True 
         while connecting_to_tcp_server: #Starting the TCP Connection
             with socket(AF_INET, SOCK_STREAM) as s:
-                s.connect((ip, content[2]))
-                s.sendall('Fingers Of Doom4'.encode()) #Sending Team name
+                s.connect((ip, tcp_port)) 
+                s.sendall('Fingers Of Doom'.encode()) #Sending Team name
                 data = s.recv(1024).decode()
+                time_limit=10
                 print(data)
                 start_time = time.time()
-                while time.time()-start_time < 10:
+                while time.time()-start_time < time_limit:
                     if msvcrt.kbhit():
                         s.sendall(msvcrt.getch())
                 connecting_to_tcp_server=False
@@ -42,11 +58,10 @@ def TCP_Connection(content,ip):
     except:
         pass
 def main():
-# Enable broadcasting mode
     print("client started, listening for offer requests...")
     while True:
-        content,ip=Looking_For_Server()
-        TCP_Connection(content,ip)
+        tcp_port,ip=Looking_For_Server()
+        TCP_Connection(tcp_port,ip)
 
     
 
